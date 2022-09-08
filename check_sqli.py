53c2aee08d52c79148e54b825a1d1a575b3293dd
lucsemassa <40078459+lucsemassa@users.noreply.github.com>
2022-09-08 05:21:04 +0100

    Add files via upload

---
+++ check_sqli.py
@@ -0,0 +1,14 @@
+import re
+mysql_log_file = "/var/log/mysql/mysql.log"
+pattern_mysql = re.compile("[\'][%]?\w+\'[%]?[\']")
+
+for i, line in enumerate(open(mysql_log_file)):
+    for match in re.finditer(pattern_mysql, line):    
+        print('Found on line %s: %s' % (i+1, match.group()))
+
+pattern_mysql = re.compile("[\"][%]?\w+\"[%]?[\"]")
+
+for i, line in enumerate(open(mysql_log_file)):
+    for match in re.finditer(pattern_mysql, line):    
+        print('Found on line %s: %s' % (i+1, match.group()))
+
